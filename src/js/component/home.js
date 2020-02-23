fe9813e00f5540a0fccc96ac5b14d2169277d9ff
KristinaFrants <cristina.chiticari@gmail.com>
2020-02-23 23:57:06 +0000

    new commit

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,55 @@
 import React from "react";
+import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+// function SimpleCounter(props) {
+// 	return (
+// 		<div className="bigCounter row" style={{ backgroundColor: "black" }}>
+// 			<div className="col digit col">
+// 				<i className="far fa-clock" />
+// 			</div>
+// 			<div className="four digit col">
+// 				<p>{props.digitFour % 10}</p>
+// 			</div>
+// 			<div className="three digit col">
+// 				<p>{props.digitThree % 10}</p>
+// 			</div>
+// 			<div className="two digit col">
+// 				<p>{props.digitTwo % 10}</p>
+// 			</div>
+// 			<div className="one digit col">
+// 				<p>{props.digitOne % 10}</p>
+// 			</div>
+// 		</div>
+// 	);
+// }
 
-//create your first component
-export function Home() {
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-}
+// SimpleCounter.propTypes = {
+// 	digitFour: PropTypes.number,
+// 	digitThree: PropTypes.number,
+// 	digitTwo: PropTypes.number,
+// 	digitOne: PropTypes.number
+// };
+
+// let counter = 0;
+
+// setInterval(function() {
+// 	const four = Math.floor(counter / 1000);
+// 	const three = Math.floor(counter / 100);
+// 	const two = Math.floor(counter / 10);
+// 	const one = Math.floor(counter / 1);
+// 	// console.log(four);
+// 	counter++;
+// 	//render your react application
+// 	ReactDOM.render(
+// 		<SimpleCounter
+// 			digitOne={one}
+// 			digitTwo={two}
+// 			digitThree={three}
+// 			digitFour={four}
+// 		/>,
+// 		document.querySelector("#app")
+// 	);
+// }, 10);
+
+// ReactDOM.render(<SimpleCounter />, document.querySelector("#app"));
--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
 
 //include bootstrap npm library into the bundle
 import "bootstrap";
@@ -9,7 +10,58 @@ import "bootstrap";
 import "../styles/index.scss";
 
 //import your own components
-import { Home } from "./component/home.js";
+import "./component/home.js";
 
+function SimpleCounter(props) {
+	return (
+		<div
+			className="bigCounter row"
+			style={{ backgroundColor: "transparent" }}>
+			<div className="col digit col">
+				<i className="far fa-clock" />
+			</div>
+			<div className="four digit col">
+				<p>{props.digitFour % 10}</p>
+			</div>
+			<div className="three digit col">
+				<p>{props.digitThree % 10}</p>
+			</div>
+			<div className="two digit col">
+				<p>{props.digitTwo % 10}</p>
+			</div>
+			<div className="one digit col">
+				<p>{props.digitOne % 10}</p>
+			</div>
+		</div>
+	);
+}
+
+SimpleCounter.propTypes = {
+	digitFour: PropTypes.number,
+	digitThree: PropTypes.number,
+	digitTwo: PropTypes.number,
+	digitOne: PropTypes.number
+};
+
+let counter = 0;
+
+setInterval(function() {
+	const four = Math.floor(counter / 1000);
+	const three = Math.floor(counter / 100);
+	const two = Math.floor(counter / 10);
+	const one = Math.floor(counter / 1);
+	// console.log(four);
+	counter++;
+	//render your react application
+	ReactDOM.render(
+		<SimpleCounter
+			digitOne={one}
+			digitTwo={two}
+			digitThree={three}
+			digitFour={four}
+		/>,
+		document.querySelector("#app")
+	);
+}, 10);
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<SimpleCounter />, document.querySelector("#app"));
